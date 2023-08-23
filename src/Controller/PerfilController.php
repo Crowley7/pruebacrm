366c7c8778cf9631f00b5d163b38e2c7d6fded15
crowley <elias.rey2526@gmail.com>
2023-08-23 11:25:02 -0500

    perfilv1

--- src/Controller/PerfilController.php
+++ src/Controller/PerfilController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Alumno;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class PerfilController extends AbstractController
 {
     #[Route('/perfil', name: 'app_perfil')]
-    public function index(): Response
+    public function index(UserInterface $user): Response
     {
+       
+        
+       
+        $alumno = $this->getUser();
+
         return $this->render('perfil/index.html.twig', [
-            'controller_name' => 'PerfilController',
+            'alumno' => $alumno,
+           
         ]);
     }
 }
