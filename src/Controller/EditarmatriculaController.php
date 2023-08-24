ce993ffffb782019444b5bba5c427c66ffd193f6
crowley <elias.rey2526@gmail.com>
2023-08-24 13:36:29 -0500

    editarmatricula-p1

--- src/Controller/EditarmatriculaController.php
+++ src/Controller/EditarmatriculaController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Curso;
+use App\Entity\Alumno;
+use App\Entity\Matricula;
+use App\Repository\AlumnoRepository;
+use App\Repository\CursoRepository;
+use App\Repository\MatriculaRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+#[Route('/editarmatricula')]
 class EditarmatriculaController extends VerificadorController
 {
-    #[Route('/editarmatricula', name: 'app_editarmatricula')]
-    public function index(): Response
+    #[Route('/', name: 'app_matricula_index', methods:['GET'])]
+    public function index(Request $request, CursoRepository $cursos, AlumnoRepository $alumnos): Response
+    {
+        $this->verificarAcceso();
+        $user = $this->getUser();
+        $alumno = $alumnos->findOneBy([
+            'dni' => $user->getUserIdentifier(),
+        ]);
+
+        
+        
+        return $this->render('editarmatricula/index.html.twig', [
+            'cursos' => $cursos->findAll(),
+            'alumno' => $alumno,
+        ]);
+    }
+
+    #[Route('/', name: 'app_eliminarmatricula', methods:['GET'])]
+    public function delete(CursoRepository $cursos, UserInterface $alumno): Response
     {
         $this->verificarAcceso();
         return $this->render('editarmatricula/index.html.twig', [
-            'controller_name' => 'EditarmatriculaController',
+            'cursos' => $cursos,
+            'alumno' => $alumno,
+        ]);
+    }
+
+    #[Route('/', name: 'app_nuevamatricula', methods:['GET'])]
+    public function new(CursoRepository $cursos, UserInterface $alumno): Response
+    {
+        $this->verificarAcceso();
+        return $this->render('editarmatricula/index.html.twig', [
+            'cursos' => $cursos,
+            'alumno' => $alumno,
         ]);
     }
 }
